aaec312454d242bd9f4812fd346274ff27a8f64f
Francisco Vaz <franciscovaz@MacBook-Pro-de-Francisco.local>
2020-02-24 23:36:29 +0000

    agendamento de serviço

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,10 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -20,6 +22,8 @@ routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 // upload.single('file') // upload de um unico ficheiro, de nome no req 'file'
 routes.post('/files', upload.single('file'), FileController.store);
