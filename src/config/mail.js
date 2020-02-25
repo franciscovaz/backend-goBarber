e141a28b0d361223cc53fd31870f0b0fc6d796bf
Francisco Vaz <franciscovaz@MacBook-Pro-de-Francisco.local>
2020-02-25 23:16:03 +0000

    configuração do nodemailer e templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '221d1d6623f38c',
+    pass: 'd2bb0d443d764e',
+  },
+  default: {
+    from: 'Equipa GoBarber <noreply@gobarber.com',
+  },
+};
